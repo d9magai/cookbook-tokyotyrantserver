a64965b66b300a601996c4c5f89430eab4eb9846
d9magai <kumagai@sencorp.co.jp>
2014-09-03 12:06:56 +0900

    eliminate rpm key

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -5,12 +5,12 @@ tc
 tt
 }.each do |key|
   package = node[:tokyotyrantserver][key]
-  rpm_filename = File.basename package['rpm']['url']
+  rpm_filename = File.basename package['url']
 
   # rpm download
   remote_file "#{Chef::Config['file_cache_path']}/#{rpm_filename}" do
-    source "#{package['rpm']['url']}"
-    checksum "#{package['rpm']['checksum']}"
+    source "#{package['url']}"
+    checksum "#{package['checksum']}"
   end
 
   # rpm install
