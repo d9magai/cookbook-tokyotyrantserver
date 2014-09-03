bbf75029fd491f0a40ecb6388b996e1d522af18e
d9magai <kumagai@sencorp.co.jp>
2014-09-03 15:43:09 +0900

    define basename of archive file

--- recipes/source.rb
+++ recipes/source.rb
@@ -15,19 +15,20 @@ tokyocabinet
 tokyotyrant
 }.each do |key|
   package = node[:tokyotyrantserver][key]
+  basename = "#{key}-#{package[:version]}"
 
-  remote_file "#{Chef::Config[:file_cache_path]}/#{key}-#{package[:version]}.tar.gz" do
-    source "http://fallabs.com/#{key}/#{key}-#{package[:version]}.tar.gz"
+  remote_file "#{Chef::Config[:file_cache_path]}/#{basename}.tar.gz" do
+    source "http://fallabs.com/#{key}/#{basename}.tar.gz"
   end
 
   execute "extract #{key}" do
     cwd Chef::Config[:file_cache_path]
-    command "tar xf #{key}-#{package[:version]}.tar.gz"
-    not_if {File.exist?("#{Chef::Config[:file_cache_path]}/#{key}-#{package[:version]}/")}
+    command "tar xf #{basename}.tar.gz"
+    not_if {File.exist?("#{Chef::Config[:file_cache_path]}/#{basename}/")}
   end
 
   execute "configure && make #{key}" do
-    cwd "#{Chef::Config[:file_cache_path]}/#{key}-#{package[:version]}/"
+    cwd "#{Chef::Config[:file_cache_path]}/#{basename}/"
     command <<-EOH
     ./configure
     make
