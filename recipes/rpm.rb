e719bbdff1d7e71f1b7b93cf330702e990d1126f
d9magai <kumagai@sencorp.co.jp>
2014-09-03 12:01:21 +0900

    install from rpm

---
+++ recipes/rpm.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+%w{
+tc
+tt
+}.each do |key|
+  package = node[:tokyotyrantserver][key]
+  rpm_filename = File.basename package['rpm']['url']
+
+  # rpm download
+  remote_file "#{Chef::Config['file_cache_path']}/#{rpm_filename}" do
+    source "#{package['rpm']['url']}"
+    checksum "#{package['rpm']['checksum']}"
+  end
+
+  # rpm install
+  package "#{rpm_filename} install" do
+    action :install
+    source "#{Chef::Config['file_cache_path']}/#{rpm_filename}"
+    provider Chef::Provider::Package::Rpm
+  end
+end
+
+# put init script
+template "/etc/init.d/ttservd" do
+  source "ttservctl.erb"
+  mode "0755"
+end
+
+# register as service
+service "ttservd" do
+  supports :start => true, :status => true, :stop => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
