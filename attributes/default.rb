17113f815d91ee7d44862c59c2c1bb2299c6ef80
d9magai <kumagai@sencorp.co.jp>
2014-09-03 11:46:45 +0900

    fix typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:tokyotyrantserver][:install_method] = 'soruce'
+default[:tokyotyrantserver][:install_method] = 'source'
 default[:tokyotyrantserver][:tt][:version] = "1.1.41"
 default[:tokyotyrantserver][:tc][:version] = "1.4.48"
 default[:tokyotyrantserver][:tt][:dbname] = 'casket.tch'
