efc8f620340206e4acb9ef14695357e7c48a4fbe
d9magai <kumagai@sencorp.co.jp>
2014-09-03 12:19:52 +0900

    refactor

--- recipes/source.rb
+++ recipes/source.rb
@@ -10,57 +10,45 @@ bzip2-devel
   end
 end
 
-remote_file "/usr/local/src/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}.tar.gz" do
+remote_file "#{Chef::Config['file_cache_path']}//tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}.tar.gz" do
   source "http://fallabs.com/tokyocabinet/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}.tar.gz"
 end
 
 execute "extract tokyocabinet" do
-  cwd "/usr/local/src"
-  command <<-EOH
-     tar xvfz tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}.tar.gz
-  EOH
-
-  not_if { File.exist?("/usr/local/src/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}/") }
+  cwd Chef::Config['file_cache_path']
+  command "tar xf tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}.tar.gz"
+  not_if { File.exist?("#{Chef::Config['file_cache_path']}/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}/") }
 end
 
 execute "configure && make tokyocabinet" do
-  cwd "/usr/local/src/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}/"
-
+  cwd "#{Chef::Config['file_cache_path']}/tokyocabinet-#{node[:tokyotyrantserver][:tc][:version]}/"
   command <<-EOH
     ./configure
     make
     make install
   EOH
-
   not_if { File.exist?("/usr/local/bin/tchmgr") }
-
   action :run
 end
 
-remote_file "/usr/local/src/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}.tar.gz" do
+remote_file "#{Chef::Config['file_cache_path']}/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}.tar.gz" do
   source "http://fallabs.com/tokyotyrant/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}.tar.gz"
 end
 
 execute "extract tokyotyrant" do
-  cwd "/usr/local/src"
-  command <<-EOH
-     tar xvfz tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}.tar.gz
-  EOH
-
-  not_if { File.exist?("/usr/local/src/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}") }
+  cwd Chef::Config['file_cache_path']
+  command "tar xf tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}.tar.gz"
+  not_if { File.exist?("#{Chef::Config['file_cache_path']}/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}") }
 end
 
 execute "configure && make tokyotyrant" do
-  cwd "/usr/local/src/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}/"
-
+  cwd "#{Chef::Config['file_cache_path']}/tokyotyrant-#{node[:tokyotyrantserver][:tt][:version]}/"
   command <<-EOH
     ./configure
     make
     make install
     rm /usr/local/sbin/ttservctl
   EOH
-
   not_if { File.exist?("/usr/local/bin/ttserver") }
-
   action :run
 end
