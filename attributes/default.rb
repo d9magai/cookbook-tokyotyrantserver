8313be61f9c8adfcf55f67893e9a7fce9038f228
d9magai <kumagai@sencorp.co.jp>
2014-09-03 12:02:45 +0900

    refactoring attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default[:tokyotyrantserver][:install_method] = 'source'
+default[:tokyotyrantserver][:dbname] = 'casket.tch'
+default[:tokyotyrantserver][:logfile] = "/var/log/ttserver.log"
+
 default[:tokyotyrantserver][:tt][:version] = "1.1.41"
 default[:tokyotyrantserver][:tc][:version] = "1.4.48"
-default[:tokyotyrantserver][:tt][:dbname] = 'casket.tch'
-default[:tokyotyrantserver][:tt][:logfile] = "/var/log/ttserver.log"
