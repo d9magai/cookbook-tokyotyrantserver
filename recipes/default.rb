da3e30d8a4b6884b81c8d1b918f38876241b62ee
d9magai <kumagai@sencorp.co.jp>
2014-09-04 16:37:56 +0900

    create log directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,12 @@ template "/etc/init.d/ttservd" do
   mode "0755"
 end
 
+# create log directory
+directory File.dirname(node[:tokyotyrantserver][:logfile]) do
+  action :create
+  recursive true
+end
+
 # register as service
 service "ttservd" do
   supports :status => true, :restart => true, :reload => true
