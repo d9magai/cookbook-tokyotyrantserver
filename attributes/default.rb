46c81ffcbc91d7e6407c561b8b601615595e99d0
d9magai <kumagai@sencorp.co.jp>
2014-09-10 17:38:45 +0900

    change attribute, maintain compatibility

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-default[:tokyotyrantserver][:install_method] = 'source'
-default[:tokyotyrantserver][:dbname] = 'casket.tch'
-default[:tokyotyrantserver][:logfile] = "/var/log/ttserver.log"
+default[:tt][:install_method] = 'source'
+default[:tt][:dbname] = 'casket.tch'
+default[:tt][:logfile] = "/var/log/ttserver.log"
 
-default[:tokyotyrantserver][:tokyocabinet][:version] = "1.4.48"
-default[:tokyotyrantserver][:tokyotyrant][:version] = "1.1.41"
+default[:tc][:version] = "1.4.48"
+default[:tt][:version] = "1.1.41"
 
-default[:tokyotyrantserver][:tokyocabinet][:binarypath] = '/usr/local/bin/tchmgr'
-default[:tokyotyrantserver][:tokyotyrant][:binarypath] = '/usr/local/bin/ttserver'
+default[:tc][:binarypath] = '/usr/local/bin/tchmgr'
+default[:tt][:binarypath] = '/usr/local/bin/ttserver'
