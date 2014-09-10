62a17dd150c22be9ced9854e79000f2f622d3e8e
d9magai <kumagai@sencorp.co.jp>
2014-09-10 17:50:33 +0900

    fix source recipe

--- recipes/source.rb
+++ recipes/source.rb
@@ -10,24 +10,25 @@ bzip2-devel
   end
 end
 
+packagename = {'tc' => 'tokyocabinet', 'tt' => 'tokyotyrant'}
 %w{
-tokyocabinet
-tokyotyrant
+tc
+tt
 }.each do |key|
-  package = node[:tokyotyrantserver][key]
-  basename = "#{key}-#{package[:version]}"
+  package = node[key]
+  basename = "#{packagename[key]}-#{package[:version]}"
 
   remote_file "#{Chef::Config[:file_cache_path]}/#{basename}.tar.gz" do
-    source "http://fallabs.com/#{key}/#{basename}.tar.gz"
+    source "http://fallabs.com/#{packagename[key]}/#{basename}.tar.gz"
   end
 
-  execute "extract #{key}" do
+  execute "extract #{packagename[key]}" do
     cwd Chef::Config[:file_cache_path]
     command "tar xf #{basename}.tar.gz"
     not_if {File.exist?("#{Chef::Config[:file_cache_path]}/#{basename}/")}
   end
 
-  execute "configure && make #{key}" do
+  execute "configure && make #{packagename[key]}" do
     cwd "#{Chef::Config[:file_cache_path]}/#{basename}/"
     command <<-EOH
     ./configure
